0f4763caa4f0ae70ce3f2f22c2e6abe9cc7e7e3e
JAGATH KRISHNAN A P <jagathkrishnanap10@gmail.com>
2025-07-09 16:42:58 +0530

    ecom_production0.1

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,30 +1,4 @@
-// import React from 'react'
-// import AppHeader from '../components/homepagecomponent/AppHeaders'
-// const AccountPage = () => {
-//   return (
-//     <div style={{
-//       minHeight: '100vh',
-//       backgroundColor: '#f9fafb'
-//     }}>
-//       <AppHeader />
-//       <div className="container mx-auto p-4">
-//         <h1 className="text-2xl font-bold mb-4">Your Account</h1>
-//         <p className="text-gray-700">This is your account page.</p>
-//         </div>
-
-//     </div>
-//   )
-// }
-
-// export default AccountPage
-
-
-
-
-
-
-
-
+// src/pages/AccountPage.jsx
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import AppHeader from '../components/homepagecomponent/AppHeaders'
--- src/pages/YourOrderPage.jsx
+++ src/pages/YourOrderPage.jsx
@@ -1,27 +1,3 @@
-// import React from 'react'
-// import AppHeader from '../components/homepagecomponent/AppHeaders'
-// const AccountPage = () => {
-//   return (
-//     <div style={{
-//       minHeight: '100vh',
-//       backgroundColor: '#f9fafb'
-//     }}>
-//       <AppHeader />
-//       <div className="container mx-auto p-4">
-//         <h1 className="text-2xl font-bold mb-4">Your Orders</h1>
-//         <p className="text-gray-700">This is your orders page.</p>
-//         </div>
-
-//     </div>
-//   )
-// }
-
-// export default AccountPage
-
-
-
-
-
 // catagory creation page
 import React, { useState } from 'react';
 
